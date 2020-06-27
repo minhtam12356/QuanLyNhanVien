b6c7af3cf354ceb648fdc60b44e3e15197f38704
tungnguyen741 <tam26918@gmail.com>
2020-06-27 19:46:11 +0700

    Add

---
+++ QuanLyNhanVienApp/B_NhanVien/B_NhanVien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Data_Tier;
+
+namespace Business_Tier
+{
+    public class B_NhanVien
+    {
+        D_NhanVien objNhanVien = new D_NhanVien();
+        public DataTable getTableNhanVien()
+        {
+            return objNhanVien.getAllTable("NhanVien").Tables["NhanVien"];
+        }
+        public void CapNhatTable_NhanVien(DataTable tbNhanVien)
+        {
+            objNhanVien.updateData(tbNhanVien, "NhanVien");
+        }
+
+        
+    }
+}
