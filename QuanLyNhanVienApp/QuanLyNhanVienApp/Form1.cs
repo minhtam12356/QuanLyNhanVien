ba5ca40dd18ec33956716fb4ebe818958231460f
tungnguyen741 <tam26918@gmail.com>
2020-06-30 11:13:03 +0700

    Add, Edit, Delete

--- QuanLyNhanVienApp/QuanLyNhanVienApp/Form1.cs
+++ QuanLyNhanVienApp/QuanLyNhanVienApp/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Business_Tier;
 using Data_Tier;
@@ -30,6 +23,7 @@ namespace QuanLyNhanVienApp
         {
             LoadComboBox();
             busNV.layDSDonHang(dGVNhanVien);
+
         }
 
 
